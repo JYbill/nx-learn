e8e1c130f4601d28a1f96359ca8da7ba195a1eb8
xiaoqinvar <17683860320m0@sina.cn>
2025-08-21 11:16:08 +0800

    成功调试@nx/js

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -6,5 +6,27 @@ export default {
     '^.+\\.ts$': ['ts-jest', { tsconfig: './tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/api'
+  coverageDirectory: '../../coverage/apps/api',
+  testMatch: [
+    "./**/*.spec.ts",
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '\\.pnp\\.[^\\/]+$',
+    // 忽略所有 dist 目录
+    '.*/dist/.*',
+    // 或者更具体地忽略 workspace 包的 dist 目录
+    'libs/.*/dist/.*',
+    'apps/.*/dist/.*'
+  ],
+  modulePathIgnorePatterns: [
+    '<rootDir>/libs/.*/dist',
+    '<rootDir>/apps/.*/dist',
+    // 或者更通用的模式
+    '.*/dist/.*'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '.*/dist/.*'
+  ]
 };
