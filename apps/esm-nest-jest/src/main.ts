1880caad7e9ec82b32f039c28e2a1c4d84285811
xiaoqinvar <17683860320m0@sina.cn>
2025-10-29 21:05:29 +0800

    🧪 test: definite quickly check all project tasks.

--- apps/esm-nest-jest/src/main.ts
+++ apps/esm-nest-jest/src/main.ts
@@ -13,7 +13,6 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(1234, 5);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
